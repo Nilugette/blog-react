7df3265af64bdda28bae7a0df0d1377468cd4a9d
Nilugette <nilugette@yahoo.fr>
2021-03-04 16:59:07 +0100

    List Building

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,5 @@ import api from '../api/jsonPlaceholder'
 
 export const fetchPosts = () => async dispatch => {
     const response = await api.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: response})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data})
 }
