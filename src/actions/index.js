01ca6da9fe4d917909e60a8c8d214c0ac2dd5097
Nilugette <nilugette@yahoo.fr>
2021-03-04 15:26:42 +0100

    Deal with async actions Creator with Thunk middleware

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import api from '../api/jsonPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+    const response = await api.get('/posts')
+    dispatch({ type: 'FETCH_POSTS', payload: response})
+}
