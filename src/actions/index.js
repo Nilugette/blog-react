60494639b440270b5c4e11584a93ecd7601d0605
Nilugette <nilugette@yahoo.fr>
2021-03-05 13:48:35 +0100

    Refactor with chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,15 @@ import api from '../api/jsonPlaceholder'
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts())
     //map lodash function and uniq to find the uniq id:
-    const userIds = _.uniq(_.map(getState().posts, 'userId'))
-    userIds.forEach(id => dispatch(fetchUser(id)))
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    // userIds.forEach(id => dispatch(fetchUser(id)))
 
+    //refactor with chain :
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 }
 
 export const fetchPosts = () => async dispatch => {
